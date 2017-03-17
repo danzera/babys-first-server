37d65d49acce5cccca40fcccd834d09ba8fc4b7c
Dan Zera <dczera@gmail.com>
2017-03-17 01:40:00 -0500

    add AJAX call to get cars array from the server

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ var express = require('express');
 var app = express();
 // specify a port that our server will listen on
 var port = 5000;
+// global variable cars[] to store user input
+// data will be coming over from client side form
+var cars = [{make: 'Kia', model: 'Seoul'}];
 
 // specify a default file to serve up
 // all static files are located in the public folder
@@ -11,6 +14,16 @@ app.use(express.static('server/public', {
   // a path to the defaul index file is then provided (again, I think)
   index: 'views/index.html'
 }));
+
+// what to do for a 'GET' /cars request by the client?
+app.get('/cars', function(req, res) {
+  // log msg to the server console
+  console.log('/cars get request by client - sending back: ', cars);
+  // send cars[] array back to the client
+  res.send(cars);
+});
+// add 'POST' request to handle reception of new car objects
+// app.post()
 
 // tell the server to listen on the port we specified (5000, in this case)
 app.listen(port);
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,43 @@
 $(document).ready(function() {
-console.log('jQuery totes sourced');
+  // make sure our client.js and jquery.js files are both sourced
+  console.log('jQuery totes sourced');
+  // what happens on a submit click event?
+  // could just use 'e' instead of event
+  $('.cars').on('submit', function(event) {
+    // need to prevent the 'submit' button from auto-refreshing the page
+    event.preventDefault();
+    // car info entered by user will be stored in an object
+    var car = {};
+    // add properties to the object with values equal to the user's input
+    car.make = $('#make').val();
+    car.model = $('#model').val();
+    // push our car object into the cars array
+    // cars.push(car);
+    // add console.log below, then...
+    // !!!---!!!---!!!---TEST---!!!---!!!---!!!
+    // log the object to the console
+    console.log(car);
+    // log the full array to the console (NOW LOCATED ON SERVER)
+    //console.log(cars);
+
+    // add AJAX call and send over car object to the server
+    // console.log('cars submit AJAX post request to the server a success - huzzah!');
+
+    // add AJAX call to get cars[] array from the server
+    $.ajax({
+      type: 'GET', // requst type
+      url: '/cars', // route
+      success: function(data) {
+        // if successful, log the data to the console
+        console.log('AJAX get /cars request a success: ', data);
+        // empty the div that holds our cars data on the DOM
+        //$('#carsTable').empty();
+        // loop through the array of cars that was returned by the server
+        //for (var i = 0; i < data.length; i++) {
+          // append the data to the DOM
+          //$('#carsTable').append('<p>Make: ' + data[i].make + 'Model: ' + data[i].model + '</p>');
+        //}
+      }
+    });
+  });
 });
