1515605696b925d54e1bb749f1ceae6a058e3c35
Dan Zera <dczera@gmail.com>
2017-03-17 12:13:03 -0500

    factor out event listeners

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   // make sure our client.js and jquery.js files are both sourced
   console.log('jQuery totes sourced');
+  // add event listeners
+  addEventListeners();
+}); // END of document.ready
+
+function addEventListeners() {
   // what happens on a submit click event?
   // could just use 'e' instead of event
   $('.userInput').on('submit', function(event) {
@@ -20,7 +25,7 @@ $(document).ready(function() {
       type: 'POST', // request type
       url: '/cars', // route
       data: car,
-      success: function(res) {
+      success: function() {
         console.log('/cars POST request to the server successful - huzzah!');
       }
     }); // END AJAX '/cars' 'POST' request
@@ -47,4 +52,40 @@ $(document).ready(function() {
       } // END success function()
     }); // END AJAX '/cars' 'GET' request
   }); // END cars-onSubmit event handler
-}); // END of document.ready
+}
+
+function postNewCar() {
+  // AJAX call to send user's input of new car object over to the server
+  $.ajax({
+    type: 'POST', // request type
+    url: '/cars', // route
+    data: car,
+    success: function() {
+      console.log('/cars POST request to the server successful - huzzah!');
+    }
+  }); // END AJAX '/cars' 'POST' request
+} // END postNewCar() function
+
+function getCars() {
+  // AJAX call to get cars[] array from the server
+  $.ajax({
+    type: 'GET', // requst type
+    url: '/cars', // route
+    success: function(res) { // res === cars[] array from server
+      // if successful, log the data to the console
+      console.log('AJAX get /cars request a success: ', res);
+      // empty the table that holds our cars data on the DOM
+      $('#carsTable').empty();
+      // create the table header
+      $('#carsTable').append('<thead><tr><th>MAKE</th><th>MODEL</th></tr></thead><tbody></tbody>');
+      // loop through the cars[] array that was returned by the server
+      for (var i = 0; i < res.length; i++) {
+        // append the data to the body of the table to display on the DOM
+        // last chiled of the carsTable is <tbody>
+        var $parent = $('#carsTable').children().last();
+        // append elements of the cars[] array to the table body
+        $parent.append('<tr><td>' + res[i].make + '</td><td>' + res[i].model + '</td></tr>');
+      } // END for-loop through the cars[] array
+    } // END success function()
+  }); // END AJAX '/cars' 'GET' request
+}
